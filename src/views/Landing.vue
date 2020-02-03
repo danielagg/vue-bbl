fcb976f5117c8fd30a6c4bbbfaac78ac8b25cafe
Daniel Agg <daniel.agg@outlook.com>
2020-02-03 17:01:33 +0100

    Cleanup

---
+++ src/views/Landing.vue
@@ -0,0 +1,15 @@
+<template>
+  <BounceLoader />
+</template>
+
+<script>
+// @ is an alias to /src
+import BounceLoader from "vue-spinner/src/BounceLoader.vue";
+
+export default {
+  name: "home",
+  components: {
+    BounceLoader
+  }
+};
+</script>
